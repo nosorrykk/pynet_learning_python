03f4ceb2d918207a89e41b74956fc0ef0fa1876a
nosorrykk <kloudknowledge@gmail.com>
2025-12-11 20:37:05 -0700

    Adding pynet lesson 1 solution to git repo

---
+++ lesson1_exercise1.py
@@ -0,0 +1,13 @@
+
+sf_gw1 = "172.31.255.1/24"
+sf_gw2 = input("Please input 'sf_gw2' IP address: ")
+
+header = "-" * 20
+header1 = "sf_gw1"
+header2 = "sf_gw2"
+
+print()
+print(f"{header1:^20} {header2:^20}")  
+print(f"{header:^20} {header:^20}")
+print(f"{sf_gw1:^20} {sf_gw2:^20}")
+print()
