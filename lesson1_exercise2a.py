f5c2a575511f0f9804354c45d672bf7b2f70ea97
nosorrykk <kloudknowledge@gmail.com>
2025-12-13 12:15:30 -0700

    Add lesson1_exercise 2 files to repo

---
+++ lesson1_exercise2a.py
@@ -0,0 +1,5 @@
+dc_location = input("Enter data center location: ")
+dc_location = dc_location.upper()
+
+print(dc_location)
+
---
+++ lesson1_exercise2b.py
@@ -0,0 +1,6 @@
+dc_location = input("Enter data center location: ")
+
+print(repr(dc_location))
+
+dc_location = dc_location.upper().strip()
+print(dc_location)
