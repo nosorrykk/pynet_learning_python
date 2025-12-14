6251f611a63313a0c1cd15a0ef5f8c7a762827a7
nosorrykk <kloudknowledge@gmail.com>
2025-12-14 15:12:48 -0700

    Adding lesson1 excercise 5 solutions completed

---
+++ lesson1_exercise5_format_method.py
@@ -0,0 +1,7 @@
+ip_addr = "10.12.17.1"
+mac_addr = "0024.c4e9.48ae"
+joiner = "-->"
+
+string_conc_output = "{} {} {}".format(ip_addr, joiner, mac_addr)
+print(string_conc_output)
+
---
+++ lesson1_exercise5_string_conc.py
@@ -0,0 +1,7 @@
+ip_addr = "10.12.17.1"
+mac_addr = "0024.c4e9.48ae"
+
+string_conc_output = ip_addr + " --> " + mac_addr
+
+print(string_conc_output)
+
